44d322a788c046052e16dd6b6438b9837b39219d
Natália Favaro Cavicchioli <na.cavicchioli@gmail.com>
2022-12-16 11:20:00 -0300

    deploying settings

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web-production-71ca.up.railway.app", "0.0.0.0"]
 
 
 # Application definition
