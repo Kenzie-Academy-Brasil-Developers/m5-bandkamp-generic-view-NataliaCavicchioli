7ea1d751e263fe6197568973758fc68a71c865f3
Natália Favaro Cavicchioli <na.cavicchioli@gmail.com>
2022-12-16 11:53:05 -0300

    new try: deploying settings

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -110,10 +110,8 @@ DATABASES = {
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL:
-    db_from_env = dj_database_url.config(
-        default=DATABASE_URL, conn_max_age=500, ssl_require=True
-    )
-    DATABASES["default"].update(db_from_env)
+    db_deploy = dj_database_url.config(default=DATABASE_URL)
+    DATABASES["default"].update(db_deploy)
     DEBUG = False
 
 if not DEBUG:
